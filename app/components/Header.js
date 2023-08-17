3e105cd49de549a631e12f7b675e6e5d592e72b4
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-17 08:30:59 +0200

    header css driler

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const Header = () => {
     return ( 
-        <header className="flex flex-col justify-center text-center bg-[#BA324F] p-[2rem] gap-10">
+        <header className="flex justify-center text-center bg-[#BA324F] p-[2rem] gap-10">
             <section className="border-2 border-white p-[10rem]">
-                <img className="h-[10rem] w-[10rem] place-self-center rounded-2xl" src="/img/pink-me-2.png" alt="" />
-                <img className="place-self-center h-[10rem] " src="/img/navn.png" alt="Logo" />
+                <img className="flex items-center justify-center h-[10rem] w-[10rem] rounded-2xl" src="/img/pink-me-2.png" alt="Logo" />
+                <img className="flex place-self-center h-[10rem]" src="/img/navn.png" alt="Name" />
                 <nav className="">
                     <Link className="pl-5 text-3xl hover:underline" href="/">Home Page</Link>    
                     <Link className="pl-5 text-3xl hover:underline" href="/info-page">Info Page</Link>
