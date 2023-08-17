d220a8456b1ff7086efb600d1eb4024d964a2a03
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-17 11:07:22 +0200

    main page almost done + adding pictures

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,16 +1,22 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation"
+
 
 
 const Header = () => {
+
+    const pathname = usePathname()
+
     return ( 
         <header className="flex justify-center text-center bg-[#BA324F] p-[2rem] gap-10">
             <section className="border-2 border-white p-[10rem]">
                 <img className="flex items-center justify-center h-[10rem] w-[10rem] rounded-2xl" src="/img/pink-me-2.png" alt="Logo" />
                 <img className="flex place-self-center h-[10rem]" src="/img/navn.png" alt="Name" />
                 <nav className="">
-                    <Link className="pl-5 text-3xl hover:underline" href="/">Home Page</Link>    
-                    <Link className="pl-5 text-3xl hover:underline" href="/info-page">Info Page</Link>
-                    <Link className="pl-5 text-3xl hover:underline" href="/gallery">Gallery</Link>
+                    <Link className={pathname == "/" ? "pl-5 text-3xl hover:underline text-[#4BA3C3]" : "text-white pl-5 text-3xl hover:underline"} href="/">Home Page</Link>    
+                    <Link className={pathname == "/info-page" ? "pl-5 text-3xl hover:underline text-[#4BA3C3]" : "text-white pl-5 text-3xl hover:underline"} href="/info-page">Info Page</Link>
+                    <Link className={pathname == "/gallery" ? "pl-5 text-3xl hover:underline text-[#4BA3C3]" : "text-white pl-5 text-3xl hover:underline"} href="/gallery">Gallery</Link>
                 </nav>
             </section>
         </header>
--- app/page.js
+++ app/page.js
@@ -33,11 +33,35 @@ export default function Home() {
       </section>
 
 
+      <section className='grid grid-cols-3 pb-[10rem] gap-5'>
+        <article className='border-[0.2rem] border-[#CCE4F6] p-5 text-center'>
+          <img className=' h-[11rem] object-cover' src="/img/MyMovie.png" alt="Movie Website" />
+          <h2 className='text-2xl p-3'>My Movie Webpage</h2>
+          <p>This website was my first attempt at using an API fully on a whole website. It's made with clean JavaScript and normal CSS.</p>
+          <Link className='underline text-xl text-[#4BA3C3]' href="https://mymovies-wess.netlify.app/">Link to MyMovie</Link>
+        </article>
+        <article className='border-[0.2rem] border-[#CCE4F6] p-5 text-center'>
+          <img className=' h-[11rem] object-cover' src="/img/movibes.png" alt="Movie Website" />
+          <h2 className='text-2xl p-3'>Movibes Webpage</h2>
+          <p>This website is a sequal to my first movie site, MyMovie. This site is made with Next.js and Tailwind CSS.</p>
+          <Link className='underline text-xl text-[#4BA3C3]' href="https://weslyn-movibes.netlify.app/">Link to Movibes</Link>
+        </article>
+        <article className='border-[0.2rem] border-[#CCE4F6] p-5 text-center'>
+          <img className=' h-[11rem] object-cover' src="/img/Portfolio4.png" alt="Movie Website" />
+          <h2 className='text-2xl p-3'>My 4th Portolio</h2>
+          <p>This is my fourth attempt at makin my portfolio. This webpage is made with vanilla React and CSS components.</p>
+          <Link className='underline text-xl text-[#4BA3C3]' href="https://weslyn-portfolio-4.netlify.app/">Link to 4th Portfolio</Link>
+        </article>
+      </section>
+
+
       <section>
         <article>
-          <h1>Ready to Dive into Weslyn's World?</h1>
-          <button><Link href="/gallery">Gallery</Link></button>
-          <button><Link href="/info-page">Info Page</Link></button>
+          <h1 className='text-[2.5rem] pb-3'>Ready to Dive into Weslyn's World?</h1>
+          <div className='flex gap-3'>
+          <button className='bg-white text-black px-10 py-3 rounded-xl text-xl'><Link href="/info-page">Info Page</Link></button>
+          <button className='bg-[#CCE4F6] text-black px-10 py-3 rounded-xl text-xl'><Link href="/gallery">Gallery</Link></button>
+          </div>
         </article>
       </section>
     </main>
