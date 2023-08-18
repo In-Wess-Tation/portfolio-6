98dc6c772cf1247083bee304d731d619c3274387
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-18 13:39:00 +0200

    Footer draft one

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
 
 
 const Footer = () => {
     return ( 
-        <footer>
-            
+        <footer className="p-10 bg-gray-600">
+            <article>
+                <div className="flex flex-col">
+                <Link className="text-[1.5rem] underline text-[#CCE4F3]" href="https://www.instagram.com/wessltoft/">My personal Instagram</Link>
+                <Link className="text-[1.5rem] underline text-[#CCE4F3]" href="https://www.instagram.com/inwesstation/">My art Isntagram</Link>
+                </div>
+                <p className="text-[1.5rem] text-[#4BA3C3]">Mobile Number: +45 27 83 55 59</p>
+                <p className="text-[1.5rem] text-[#4AB3C3]">Email: weslyn-l-toft@gmail.com</p>
+            </article>
         </footer>
      );
 }
