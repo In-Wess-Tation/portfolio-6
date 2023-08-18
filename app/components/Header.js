ee598ebbf4f9c20c0a56e4262d04663ac14bab20
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-18 13:50:23 +0200

    update to the header

--- app/components/Header.js
+++ app/components/Header.js
@@ -15,10 +15,9 @@ const Header = () => {
 
     return ( 
         <header className="flex justify-center text-center bg-[#BA324F] p-[2rem] gap-10">
-            <section className="border-2 border-white p-[10rem]">
-                <img className="flex items-center justify-center h-[10rem] w-[10rem] rounded-2xl" src="/img/pink-me-2.png" alt="Logo" />
+            <section className="border-2 border-white p-[10rem] py-[15rem]">
                 <img className="flex place-self-center h-[10rem]" src="/img/navn.png" alt="Name" />
-                <nav className="">
+                <nav>
                     <Link className={pathname == "/" ? `pl-5 text-3xl hover:underline text-[#4BA3C3] ${fatface.className}` : `text-white pl-5 text-3xl hover:underline ${fatface.className}`} href="/">Home Page</Link>    
                     <Link className={pathname == "/gallery" ? `pl-5 text-3xl hover:underline text-[#4BA3C3] ${fatface.className}` : `text-white pl-5 text-3xl hover:underline ${fatface.className}`} href="/gallery">View Artworks</Link>
                     <Link className={pathname == "/info-page" ? `pl-5 text-3xl hover:underline text-[#4BA3C3] ${fatface.className}` : `text-white pl-5 text-3xl hover:underline ${fatface.className}`} href="/info-page">About Weslyn</Link>
